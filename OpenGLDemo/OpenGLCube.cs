79675887eecab16e3432e3736cbb03e1df3fb32c
Simon Burfield <simon.burfield@o2.com>
2015-06-29 13:40:16 +0100

    Now rendering a texture works

---
+++ OpenGLDemo/OpenGLCube.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace OpenGLDemo
+{
+	public class OpenGLCube : ContentPage
+	{
+		public OpenGLCube ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # OpenGLXamarin
 Here is an example of Xamarin Forms and OpenGLView.  This works for iOS and Android.
+
+Built using Xamarin Studio for Mac
